149adef5315b2f89a5d04e8471a6e3b5acefb982
Lopatkin Daniil <daniil199525@gmail.com>
2016-04-08 12:22:40 +0300

    Reinitialize files

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include"vector.h"
+
+int main(void)
+{
+	std::vector<double> k;
+	vect<double> b(0);
+	k.push_back(1.0);
+	k.push_back(2.0);
+	vect<double> a(k);
+	std::cout <<"Vector A "<< a << std::endl;
+	std::cout <<"Enter A addition " << std::endl;
+	std::cin >> a;
+	std::cout <<"Enter vector B " << std::endl;
+	std::cin >> b;
+	std::cout <<"New Vector A "<< a << std::endl;
+	std::cout <<"A*B " <<  a.scalarMult(b) << std::endl;
+	return 0;
+}
